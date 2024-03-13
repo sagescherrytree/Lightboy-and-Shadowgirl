d78082b1ee571dea56bf0f2cfb8b276381edf2f1
Nicholas Liu <liunicholas6@gmail.com>
2024-03-13 17:44:18 -0400

    Removal of all C++

--- Source/LightboyShadowgirl/LightboyShadowgirl.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "LightboyShadowgirl.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LightboyShadowgirl, "LightboyShadowgirl" );
